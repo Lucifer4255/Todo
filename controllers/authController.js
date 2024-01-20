5cd2cccc8b8597de0f2cb301c71a5fb562de4e06
Lucifer4255 <deysuhotra@gmail.com>
2024-01-20 22:16:27 +0530

    fifth commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const {promisify} = require('util');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync')
-
+const AppError = require('../../utils/appError')
 exports.signup = catchAsync(async(req,res,next) => {
     const newUser = await User.create(req.body);
 
@@ -18,4 +18,15 @@ exports.signup = catchAsync(async(req,res,next) => {
             user: newUser
         }
     });
+});
+
+exports.login = catchAsync(async(req, res, next) =>{
+    const {username,password} = req.body
+    if(!username || !password) return next(new AppError('Please provide email and password!',400));
+
+    const token = '';
+    res.status(200).json({
+        status: 'success',
+        token
+    })
 });
